10d0adb13b5f1a59ee5f57d18ba1956d33a53de0
hfionte <hfionte@gmail.com>
2017-06-12 12:07:51 -0400

    js testing

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 
 $(document).ready(function() {
-    console.log('hello world');
     
-    
+    $('.filters select').on('change', function(e) {
+        console.log(e.target.value);
+        var filterName = $(e.target).data('filtername');
+        var filterValue = $(e.target).val();
+        $('.item').each(function(items, item) {            
+            if ($(item).data(filterName) === filterValue) {
+                $(item).show();
+            } else {
+                $(item).hide();
+            }
+        });
+    });
     
 });
